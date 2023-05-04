34b93703662f80ebb25a4fea0563debb2dbb07db
Jolene Kearse <pharaohnutz@gmail.com>
2023-05-04 15:05:06 -0400

    style: add tailwind config & fonts

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: ["class", '[data-mode="dark"]'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
