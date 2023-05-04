3f7df16a70519af315193e5c3255a350d30800a9
Jolene Kearse <pharaohnutz@gmail.com>
2023-05-04 15:26:51 -0400

    ci: update eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,5 @@
 module.exports = {
+  root: true,
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
@@ -6,7 +7,7 @@ module.exports = {
     'plugin:react-hooks/recommended',
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parserOptions: { ecmaVersion: 'latest', ecmaFeatures: {jsx: true}, sourceType: 'module' },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
