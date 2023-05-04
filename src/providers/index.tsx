a538d9378a13e2649a6d06106ca4fdafd12894f2
Prince Muel <vansomecsam@gmail.com>
2023-05-04 21:50:04 +0100

    feat: setup query client providers
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,3 +1,6 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import * as React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { ThemeProvider } from '../lib';
 import { router } from '../routes';
@@ -5,10 +8,28 @@ import { router } from '../routes';
 interface Props {}
 
 const Providers = (props: Props) => {
+  const [client] = React.useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            // cacheTime: 1000 * 60 * 60 * 24, // 24 hours
+            staleTime: 1000 * 60 * 3, // 2 min
+            retry: 1,
+            refetchOnMount: true,
+            refetchOnReconnect: true,
+            refetchOnWindowFocus: true,
+          },
+        },
+      })
+  );
   return (
-    <ThemeProvider>
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <QueryClientProvider client={client}>
+      <ThemeProvider>
+        <ReactQueryDevtools position='bottom-right' initialIsOpen={false} />
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
 
