ab7ed681989ef3c11dc0c92ab0c2feba4e60d538
Prince Muel <vansomecsam@gmail.com>
2023-05-04 20:08:34 +0100

    feat: add fetcher util function
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ src/utils/fetch.ts
@@ -0,0 +1,19 @@
+export function fetcher<TData, TVariables>(query: string, variables?: TVariables) {
+  return async (): Promise<TData> => {
+    const response = await fetch("", {
+    method: "POST",
+    ...({"headers":{"Content-Type":"application/json"}}),
+      body: JSON.stringify({ query, variables }),
+    });
+
+    const json = await response.json();
+
+    if (json.errors) {
+      const { message } = json.errors[0];
+
+      throw new Error(message);
+    }
+
+    return json.data;
+  }
+}
