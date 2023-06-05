91a438ad85f4478b74945f69b900caacbbea2242
Jolene Kearse <pharaohnutz@gmail.com>
2023-06-05 13:13:52 -0400

    feat: add sidebar & styled mob

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import svgr from "vite-plugin-svgr"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr({ exportAsDefault: true })],
   server: {port: 3000, open: true},
   resolve: {alias: [{find: '@src', replacement: '/src/'}]}
 })
