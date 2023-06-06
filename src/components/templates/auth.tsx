4cbea056e1366e997d2dd409842bb148c66d9987
Prince Muel <vansomecsam@gmail.com>
2023-06-06 21:13:59 +0100

    feat: add auth template
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ src/components/templates/auth.tsx
@@ -0,0 +1,21 @@
+import clsx from 'clsx';
+
+interface Props {
+  className?: string;
+  children: React.ReactNode;
+}
+
+const AuthFormTemplate = ({ children, className }: Props) => {
+  return (
+    <section
+      className={clsx(
+        'flex h-full items-center justify-center px-8',
+        className
+      )}
+    >
+      {children}
+    </section>
+  );
+};
+
+export { AuthFormTemplate };
