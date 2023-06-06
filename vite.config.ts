95b4f2739717f230d76b16b14222d9c433a1bac0
Prince Muel <vansomecsam@gmail.com>
2023-06-06 21:41:41 +0100

    ci: setup base in vite config
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import svgr from "vite-plugin-svgr"
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr({ exportAsDefault: true })],
-  server: {port: 3000, open: true},
-  resolve: {alias: [{find: '@src', replacement: '/src/'}]}
-})
+  server: { port: 3000, open: true },
+  resolve: { alias: [{ find: '@src', replacement: '/src/' }] },
+  base: '/movie-app/',
+});
