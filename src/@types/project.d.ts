444551f68107b9db0a2933624cb8c014f0eaf3f2
Prince Muel <vansomecsam@gmail.com>
2023-07-11 00:42:37 +0100

    feat: update project types
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/@types/project.d.ts
+++ src/@types/project.d.ts
@@ -1,10 +1,24 @@
 interface IParams {
-  [key: string]: string | undefined;
+  readonly [key: string]: string | undefined;
 }
 
 interface IRoutesProps extends Array<IRoute> {}
 interface IRoute {
   alt: string;
   href: string;
-  icon: (props: IconProps) => JSX.Element;
+
+  icon: IconRFCType;
 }
+
+interface IconProps extends React.ComponentPropsWithoutRef<"svg"> {}
+type IconRFCType = (props: IconProps) => React.JSX.Element;
+type ClassValue =
+  | ClassArray
+  | ClassDictionary
+  | string
+  | number
+  | null
+  | boolean
+  | undefined;
+type ClassDictionary = Record<string, any>;
+type ClassArray = ClassValue[];
