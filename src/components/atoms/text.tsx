aa34d84f5da45e55e349b6aec8fe9c394ace451c
Prince Muel <vansomecsam@gmail.com>
2023-07-11 02:21:27 +0100

    feat: scaffold text component
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/components/atoms/text.tsx
+++ src/components/atoms/text.tsx
@@ -1,10 +1,78 @@
+import { cn } from "@/lib";
+import type { VariantProps } from "cva";
+import { cva } from "cva";
+
+const textVariants = cva("", {
+  defaultVariants: {
+    variant: "primary",
+    size: "base",
+    weight: "medium",
+  },
+  variants: {
+    variant: {
+      primary: "text-black",
+      "primary/50": "text-black/50",
+      inverted: "text-white",
+      secondary: "text-brand-500",
+    },
+    size: {
+      xs: "text-200 tracking-100",
+      sx: "text-300 tracking-700",
+      base: "text-400 leading-300",
+      sm: "text-500 tracking-300",
+      md: "text-600 tracking-500",
+      lg: "text-700 tracking-600",
+      xl: "text-800 tracking-200",
+      "2xl": "text-900 tracking-400",
+      "3xl": "text-xl tracking-600",
+    },
+
+    weight: {
+      bold: "font-bold",
+      medium: "font-medium",
+      regular: "font-normal",
+    },
+    uppercase: {
+      true: "uppercase",
+    },
+  },
+  compoundVariants: [
+    {
+      variant: ["primary", "secondary", "inverted"],
+      size: ["3xl", "2xl", "xl", "lg", "md", "sm", "sx", "xs"],
+      weight: "bold",
+      class: "uppercase",
+    },
+  ],
+});
+
+interface TextVariants extends VariantProps<typeof textVariants> {}
+const text = (variants: TextVariants, className = "") =>
+  cn(textVariants(variants), className);
+
+type TextProps<E extends React.ElementType = "p"> = ElementProps<E> &
+  TextVariants;
+
 const Text = <E extends React.ElementType = "p">({
+  as: As,
+  variant,
+  weight,
+  size,
+  uppercase,
+  className,
   children,
-  as,
   ...rest
-}: ElementProps<E>) => {
-  const RenderedElement = as || "p";
-  return <RenderedElement {...rest}>{children}</RenderedElement>;
+}: TextProps<E>) => {
+  const Rendered = As || "p";
+
+  return (
+    <Rendered
+      className={text({ variant, weight, size, uppercase }, className)}
+      {...rest}
+    >
+      {children}
+    </Rendered>
+  );
 };
 
 export { Text };
