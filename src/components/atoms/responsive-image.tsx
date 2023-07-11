2288e8e0d5f067a9ff9d3c6f0d5a45da02a2fcb9
Prince Muel <vansomecsam@gmail.com>
2023-07-11 03:37:05 +0100

    feat: add responsive image component
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ src/components/atoms/responsive-image.tsx
@@ -0,0 +1,36 @@
+import { cn } from "@/lib";
+
+interface Props
+  extends React.DetailedHTMLProps<
+    React.ImgHTMLAttributes<HTMLImageElement>,
+    HTMLImageElement
+  > {
+  className?: string;
+  imageClassName?: string;
+}
+
+// !! Refactor this component later
+const ResponsiveImage = ({
+  alt,
+  src,
+  className,
+  width = 700,
+  height = 475,
+  loading = "lazy",
+  imageClassName,
+  ...rest
+}: Props) => (
+  <figure className={cn("w-full", className)}>
+    <img
+      src={src}
+      alt={alt}
+      className={cn("", imageClassName)}
+      width={width}
+      height={height}
+      placeholder="blur"
+      {...rest}
+    />
+  </figure>
+);
+
+export { ResponsiveImage };
