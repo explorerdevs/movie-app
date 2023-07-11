df8cdfc188329e95a13f1eb6f63052335594a5d1
Prince Muel <vansomecsam@gmail.com>
2023-07-11 01:30:02 +0100

    feat: update fetcher method
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ src/lib/utils/fetcher.ts
@@ -0,0 +1,19 @@
+export async function request(
+  ...args: Parameters<typeof fetch>
+): Promise<unknown> {
+  try {
+  } catch (error) {}
+  const response = await fetch(...args);
+
+  if (!response.ok) {
+    return Promise.reject(new Error("Network response was not ok"));
+  }
+
+  const type = response.headers.get("content-type");
+  if (type !== "application/json") {
+    return Promise.reject(
+      new TypeError(`Malformed data. Expected 'JSON', got '${type}'`)
+    );
+  }
+  return response.json();
+}
