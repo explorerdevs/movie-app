bae0adedc8d683f280144ecf8c0cca7a021fe4cb
Prince Muel <vansomecsam@gmail.com>
2023-08-07 07:52:18 +0100

    fix: resolve svg typing issue w/ typescript
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/vite-env-override.d.ts
+++ src/vite-env-override.d.ts
@@ -6,3 +6,8 @@ interface ImportMetaEnv {
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
+
+declare module "*.svg" {
+  const content: React.FC<React.SVGProps<SVGElement>>;
+  export default content;
+}
